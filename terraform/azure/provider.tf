b88bfd3962e4763c67778040f1f5241955d8b430
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-16 20:39:42 -0300

    teste1

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-staticsite-lb-tf"
-    storage_account_name = "staticsitelbtf"
+    resource_group_name  = "rg-staticsite-lb-multicloud-tf-vitor"
+    storage_account_name = "staticsitelbmultivitor"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
