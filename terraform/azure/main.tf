c839e04594cdfec241b5bba2c2af1b766e95486a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-16 20:55:06 -0300

    teste definitivo

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-staticsite-lb-multicloud-tf-vitor"
+    name     = "rg-staticsite-multicloud-tf-vitor"
     location = "brazilsouth"
 }
 
